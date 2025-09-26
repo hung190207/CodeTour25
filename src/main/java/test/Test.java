7444ecb5c911ee57926b3bf15a616c2bddf86e15
Hung <hungcs192@gmail.com>
2025-09-26 20:27:02 +0700

    Testing commit for code tour.

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
